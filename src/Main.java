e890f5b58bd1242846280dd755441638e9005713
anastasiazheltikova <znastya2905@icloud.com>
2022-06-10 20:57:47 +0700

    готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Employee [ ] allEmployee = new Employee[10];
+        allEmployee [0] = new Employee("Иванова","Екатерина"," Михайловна", 50000,2);
+        allEmployee[1]=new Employee("Анисимова","Дарья","Геннадьевна",38000,1);
+        allEmployee[2]= new Employee("Колесников","Евгений","Петрович", 43000,4);
+        allEmployee[3]=new Employee("Крутоголова","Алина","Витальевна",62000,5);
+        allEmployee[4]=new Employee("Ковалев","Денис","Виктрович",49000,3);
+        allEmployee[5]=new Employee("Ковальчук", "Иван", "Игоревич",29000,1);
+        allEmployee[6]=new Employee("Березовская","Ангелина","Николаевна",56000,2);
+        allEmployee[7]=new Employee("Терентьева","Валентина","Ивановна",36000,5);
+        allEmployee[8]=new Employee("Красников","Виталий","Иванович",58000,3);
+        allEmployee[9]=new Employee("Овчинников","Владимир","Станиславович",41000,4);
+        /*System.out.println(allEmployee[0]);
+        System.out.println(allEmployee[1]);
+        System.out.println(allEmployee[2]);
+        System.out.println(allEmployee[3]);
+        System.out.println(allEmployee[4]);
+        System.out.println(allEmployee[5]);
+        System.out.println(allEmployee[6]);
+        System.out.println(allEmployee[7]);
+        System.out.println(allEmployee[8]);
+        System.out.println(allEmployee[9]);*/
+        countAllSalary(allEmployee);
+        printAllEmployee1(allEmployee);
+        findMinSalary(allEmployee);
+        findAverageSalary(allEmployee);
+        findMaxSalary(allEmployee);
+        printFullName(allEmployee);
+
     }
+    public static int  countAllSalary (Employee[ ] allEmployee ){
+       int sum = 0;
+        for (Employee everyEmployee:allEmployee) {
+            sum= sum+ everyEmployee.getSalary();
+        }
+        System.out.println("Общая сумма затрат на зарплаты " + sum);
+        return sum;
+    }
+
+    private static void printAllEmployee1 ( Employee [] allEmployee){
+        for (Employee everyEmployee1: allEmployee) {
+            System.out.println(everyEmployee1);
+        }
+    }
+    private static void findMinSalary (Employee [] allEmployee) {
+        int min = 100_000;
+        for ( Employee everyEmployeeMin : allEmployee){
+            if (everyEmployeeMin.getSalary() < min)
+                min=everyEmployeeMin.getSalary();
+        }
+        System.out.println("Минимальная зарплата  " + min);
+    }
+    private static void findAverageSalary (Employee [] allEmployee) {
+        int sum = countAllSalary(allEmployee);
+        int averageSalary = 0;
+        averageSalary= sum / allEmployee.length;
+        System.out.println("Средняя зарплата " + averageSalary);
+    }
+    private static void findMaxSalary ( Employee [] allEmployee) {
+        int max=0;
+        for(Employee everyEmployeeMax:allEmployee){
+            if(everyEmployeeMax.getSalary()>max)
+                max=everyEmployeeMax.getSalary();
+        }
+        System.out.println("Максимальная зарплата " + max);
+    }
+    private static void printFullName(Employee [] allEmployee){
+        for( Employee everyEmployeeFullName: allEmployee){
+
+            System.out.println( "ФИО " + everyEmployeeFullName.getSurname()+" "+everyEmployeeFullName.getName() + " " + everyEmployeeFullName.getPatronymic());
+        }
+}
 }
